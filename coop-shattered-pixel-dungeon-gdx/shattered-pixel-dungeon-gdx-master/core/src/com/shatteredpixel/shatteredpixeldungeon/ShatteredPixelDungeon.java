4384e6000445b263bb6165b7ee43db4894cfe0a2
Dartemiss <enrique.alexand.gonzalez@est.fib.upc.edu>
2019-01-02 13:27:19 +0100

    Game Commit

---
+++ coop-shattered-pixel-dungeon-gdx/shattered-pixel-dungeon-gdx-master/core/src/com/shatteredpixel/shatteredpixeldungeon/ShatteredPixelDungeon.java
@@ -0,0 +1,226 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2016 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.shatteredpixel.shatteredpixeldungeon;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.shatteredpixel.shatteredpixeldungeon.input.GameAction;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.WelcomeScene;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.RenderedText;
+import com.watabou.noosa.audio.Music;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.PDPlatformSupport;
+
+public class ShatteredPixelDungeon extends Game<GameAction> {
+	
+	//variable constants for specific older versions of shattered, used for data conversion
+	//versions older than v0.6.0b are no longer supported, and data from them is ignored
+	public static final int v0_6_0b = 185;
+	
+	public static final int v0_6_1b = 209;
+	
+	public static final int v0_6_2e = 229;
+	
+	public static final int v0_6_3b = 245;
+	
+	public static final int v0_6_4a = 252;
+
+	public static final int v0_6_5  = 260;
+	
+	public ShatteredPixelDungeon(final PDPlatformSupport<GameAction> platformSupport) {
+		super(WelcomeScene.class, platformSupport);
+		
+		Game.version = platformSupport.getVersion();
+		Game.versionCode = platformSupport.getVersionCode();
+		
+		//v0.6.2
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.rooms.secret.RatKingRoom.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.rooms.special.RatKingRoom" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.PlantsRoom.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.GardenRoom" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.rooms.special.GardenRoom.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.rooms.special.FoliageRoom" );
+		
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.WornDartTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.WornTrap" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.PoisonDartTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.PoisonTrap" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.ShockingTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.ParalyticTrap" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.ShockingTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.LightningTrap" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.GrippingTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.SpearTrap" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.BurningTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.FireTrap" );
+		
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.actors.buffs.BlobImmunity.class,
+				"com.shatteredpixel.shatteredpixeldungeon.actors.buffs.GasesImmunity" );
+
+		//v0.6.3
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.Tomahawk.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.Tamahawk" );
+
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.darts.Dart.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.Dart" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.darts.IncendiaryDart.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.IncendiaryDart" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.darts.ParalyticDart.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.CurareDart" );
+
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfCorrosion.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfVenom" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.actors.blobs.CorrosiveGas.class,
+				"com.shatteredpixel.shatteredpixeldungeon.actors.blobs.VenomGas" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Corrosion.class,
+				"com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Venom" );
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.levels.traps.CorrosionTrap.class,
+				"com.shatteredpixel.shatteredpixeldungeon.levels.traps.VenomTrap" );
+		
+		//v0.6.4
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.bags.VelvetPouch.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.bags.SeedPouch" );
+		
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.bags.MagicalHolster.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.bags.WandHolster" );
+		
+		//v0.6.5
+		com.watabou.utils.Bundle.addAlias(
+				com.shatteredpixel.shatteredpixeldungeon.items.stones.StoneOfAugmentation.class,
+				"com.shatteredpixel.shatteredpixeldungeon.items.Weightstone" );
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void create() {
+		super.create();
+
+		SPDSettings.fullscreen( SPDSettings.fullscreen() );
+		
+		Music.INSTANCE.enable( SPDSettings.music() );
+		Music.INSTANCE.volume( SPDSettings.musicVol()/10f );
+		Sample.INSTANCE.enable( SPDSettings.soundFx() );
+		Sample.INSTANCE.volume( SPDSettings.SFXVol()/10f );
+
+		Sample.INSTANCE.load(
+				Assets.SND_CLICK,
+				Assets.SND_BADGE,
+				Assets.SND_GOLD,
+
+				Assets.SND_STEP,
+				Assets.SND_WATER,
+				Assets.SND_OPEN,
+				Assets.SND_UNLOCK,
+				Assets.SND_ITEM,
+				Assets.SND_DEWDROP,
+				Assets.SND_HIT,
+				Assets.SND_MISS,
+
+				Assets.SND_DESCEND,
+				Assets.SND_EAT,
+				Assets.SND_READ,
+				Assets.SND_LULLABY,
+				Assets.SND_DRINK,
+				Assets.SND_SHATTER,
+				Assets.SND_ZAP,
+				Assets.SND_LIGHTNING,
+				Assets.SND_LEVELUP,
+				Assets.SND_DEATH,
+				Assets.SND_CHALLENGE,
+				Assets.SND_CURSED,
+				Assets.SND_EVOKE,
+				Assets.SND_TRAP,
+				Assets.SND_TOMB,
+				Assets.SND_ALERT,
+				Assets.SND_MELD,
+				Assets.SND_BOSS,
+				Assets.SND_BLAST,
+				Assets.SND_PLANT,
+				Assets.SND_RAY,
+				Assets.SND_BEACON,
+				Assets.SND_TELEPORT,
+				Assets.SND_CHARMS,
+				Assets.SND_MASTERY,
+				Assets.SND_PUFF,
+				Assets.SND_ROCKS,
+				Assets.SND_BURNING,
+				Assets.SND_FALLING,
+				Assets.SND_GHOST,
+				Assets.SND_SECRET,
+				Assets.SND_BONES,
+				Assets.SND_BEE,
+				Assets.SND_DEGRADE,
+				Assets.SND_MIMIC );
+
+		if (!SPDSettings.systemFont()) {
+			RenderedText.setFont("pixelfont.ttf");
+		} else {
+			RenderedText.setFont( null );
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+
+		Graphics.DisplayMode mode = Gdx.graphics.getDisplayMode();
+		boolean maximized = width >= mode.width || height >= mode.height;
+		
+		if (!maximized && !SPDSettings.fullscreen()){
+			SPDSettings.put(SPDSettings.KEY_WINDOW_WIDTH, width);
+			SPDSettings.put(SPDSettings.KEY_WINDOW_HEIGHT, height);
+		}
+	}
+
+	public static void switchNoFade( Class<? extends PixelScene> c ) {
+		PixelScene.noFade = true;
+		switchScene( c );
+	}
+
+	public static void switchNoFade(Class<? extends PixelScene> c, SceneChangeCallback callback) {
+		PixelScene.noFade = true;
+		switchScene( c, callback );
+	}
+
+}
---
+++ coop-shattered-pixel-dungeon-gdx/shattered-pixel-dungeon-gdx-master/desktop/src/com/watabou/pd/desktop/DesktopLauncher.java
@@ -0,0 +1,94 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2016 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.watabou.pd.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglPreferences;
+import com.badlogic.gdx.utils.SharedLibraryLoader;
+import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
+import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
+import com.watabou.input.NoosaInputProcessor;
+import com.watabou.utils.PDPlatformSupport;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		String version = DesktopLauncher.class.getPackage().getSpecificationVersion();
+		if (version == null) {
+			version = "0.6.5c";
+		}
+
+		int versionCode;
+		try {
+			versionCode = Integer.parseInt(DesktopLauncher.class.getPackage().getImplementationVersion());
+		} catch (NumberFormatException e) {
+			versionCode = 264;
+		}
+
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		if (SharedLibraryLoader.isMac) {
+			config.preferencesDirectory = "Library/Application Support/Shattered Pixel Dungeon/";
+		} else if (SharedLibraryLoader.isLinux) {
+			config.preferencesDirectory = ".shatteredpixel/shattered-pixel-dungeon/";
+		} else if (SharedLibraryLoader.isWindows) {
+			String winVer = System.getProperties().getProperty("os.name");
+			if (winVer.contains("XP")) {
+				config.preferencesDirectory = "Application Data/.shatteredpixel/Shattered Pixel Dungeon/";
+			} else {
+				config.preferencesDirectory = "AppData/Roaming/.shatteredpixel/Shattered Pixel Dungeon/";
+			}
+		}
+		// FIXME: This is a hack to get access to the preferences before we have an application setup
+		com.badlogic.gdx.Preferences prefs = new LwjglPreferences(SPDSettings.FILE_NAME, config.preferencesDirectory);
+
+		boolean isFullscreen = prefs.getBoolean(SPDSettings.KEY_WINDOW_FULLSCREEN, false);
+		//config.fullscreen = isFullscreen;
+		if (!isFullscreen) {
+			config.width = prefs.getInteger(SPDSettings.KEY_WINDOW_WIDTH, SPDSettings.DEFAULT_WINDOW_WIDTH);
+			config.height = prefs.getInteger(SPDSettings.KEY_WINDOW_HEIGHT, SPDSettings.DEFAULT_WINDOW_HEIGHT);
+		}
+
+		config.addIcon( "ic_launcher_128.png", Files.FileType.Internal );
+		config.addIcon( "ic_launcher_32.png", Files.FileType.Internal );
+		config.addIcon( "ic_launcher_16.png", Files.FileType.Internal );
+
+		// TODO: It have to be pulled from build.gradle, but I don't know how it can be done
+		config.title = "Shattered Pixel Dungeon";
+
+		new LwjglApplication(new ShatteredPixelDungeon(
+				new DesktopSupport(version, versionCode, config.preferencesDirectory, new DesktopInputProcessor())
+		), config);
+	}
+
+	private static class DesktopSupport extends PDPlatformSupport {
+		public DesktopSupport( String version, int versionCode, String basePath, NoosaInputProcessor inputProcessor ) {
+			super( version, versionCode, basePath, inputProcessor );
+		}
+
+		@Override
+		public boolean isFullscreenEnabled() {
+		//	return Display.getPixelScaleFactor() == 1f;
+			return !SharedLibraryLoader.isMac;
+		}
+	}
+}
